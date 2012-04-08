be8dc9d8cdb12198aae6ee460a8ec1bb19cdb225
Alexander Rohr <a.rohr@web.de>
2012-04-08 18:18:55 +0200

    Initial commit

---
+++ src/helloswt/HelloWorldSWT.java
@@ -0,0 +1,22 @@
+package helloswt;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class HelloWorldSWT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Hello world!");
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+	}
+}
